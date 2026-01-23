79f54b4ab0365ab0e83f36c9951e5c212ec13a8f
Najeeb <playitforpubg@gmail.com>
2026-01-24 01:23:03 +0530

    day 3&4

--- minarray.js
+++ minarray.js
@@ -1,6 +1,6 @@
-let arr=[4,7,,12,20,1,5]
+let arr=[4,7,0.5,12,20,1,5]
 let min=arr[0];
-for (let i=0;i<arr.length;i++){
+for (let i=1;i<arr.length;i++){
     if(arr[i]<min){
         min=arr[i];
     }
---
+++ oddeve.js
@@ -0,0 +1,13 @@
+let arr=[1,2,3,4,5,6,7,8,9,10];
+let odd=0;
+let even=0;
+for (let i=0;i<arr.length;i++){
+    if(arr[i]%2==0){
+        even++
+    }
+    else{
+        odd++
+    }
+}
+console.group("Odd and Even count in the array:");
+console.table({even, odd});
