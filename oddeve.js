2bc4aadade9d9b578151f8667ec63ba8f5053cd0
Najeeb <NajeebRahman@descpro.org>
2026-09-02 18:22:27 +0530

    combined Greathe than 10 or less than or equal to lvl 5: js

--- oddeve.js
+++ oddeve.js
@@ -1,6 +1,8 @@
-let arr=[1,2,3,4,5,6,7,8,9,10];
+let arr = [12, 5, 18, 7, 20, 3, 15, 8];
 let odd=0;
 let even=0;
+let greater=0;
+let lessetenOrLessr=0;
 for (let i=0;i<arr.length;i++){
     if(arr[i]%2==0){
         even++
@@ -8,20 +10,27 @@ for (let i=0;i<arr.length;i++){
     else{
         odd++
     }
+    if(arr[i]>10){
+        greater++
+    }
+    else if(arr[i]<=10){
+        lessetenOrLessr++
+    }
+    
 }
 console.group("Odd and Even count in the array:");
-console.table({even, odd}); 
+console.table({even, odd,greater, lessetenOrLessr}); 
  
 let newarr=[1,11,23,44,55,10,10,4,6]
-let greater=0;
-let lessetenOrLessr=0;
+greater=0;
+lessetenOrLessr=0;
 for(let n of newarr){
     if(n>10){
         greater++
     }
     else{
-        tenOrLess++
+        lessetenOrLessr++
     }
 }
 console.group("Numbers greater than 10:");
-console.table({greater, tenOrLess});
+console.table({greater, lessetenOrLessr});
